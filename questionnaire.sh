13947965acb9231a7ce506c5fd2e29285b8bab75
Jeremy Opriasa <opriasajbusiness@gmail.com>
2022-12-23 15:58:34 +0800

    Add the 2 files with 1 complete code and 2 not complete yet.

---
+++ questionnaire.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\n~~ Questionnaire ~~\n"
+
+
+QUESTION1="What's your name?"
+echo $QUESTION1
+read NAME
+
+
+QUESTION2="Where are you from?"
+echo $QUESTION2
+read LOCATION
+
+
+QUESTION3="What's your favorite coding website?"
+echo "\n$QUESTION3"
+read WEBSITE
+
+echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
