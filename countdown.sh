45db980741c980ba7e65dd457abe0f88ac3f0295
Jeremy Opriasa <opriasajbusiness@gmail.com>
2022-12-24 03:41:50 +0800

    Done countdown project using for and while loop in countdown challenges.

--- countdown.sh
+++ countdown.sh
@@ -2,9 +2,34 @@
 
 # Program that counts down to zero from a given argument
 
-if [[ $1 == arg1 ]]
+# Run the file using sleep (non built-in command)
+
+# To access non built-in command, use help - ls / - ls /bin - man sleep
+
+: '
+Note:
+  ./countdown.sh <argument>
+  sleep <number>
+  '
+
+echo -e "\n~~ Countdown Timer ~~\n"
+
+if [[ $1 -gt 0 ]]
 then
-  echo true
+: '
+  for (( i = $1; i >= 0; i-- ))
+  do 
+    sleep 1
+    echo $i
+  done
+  '
+  I=$1
+  while [[ $I -ge 0 ]]
+  do
+    echo $I
+    (( I-- ))
+    sleep 1
+  done
 else
-  echo false
+  echo Include a positive integer as the first argument.
 fi
